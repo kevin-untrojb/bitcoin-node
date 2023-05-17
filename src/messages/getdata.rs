b3dfe5923353f5cce3f6d53c2b25f694d7d37208
Agus <aschmidt@fi.uba.ar>
2023-05-17 01:38:19 -0300

    getdata message

--- src/messages/getdata.rs
+++ src/messages/getdata.rs
@@ -1,25 +1,31 @@
-use crate::errores::NodoBitcoinError;
+use super::messages_header::make_header;
+use crate::{blockchain::blockheader::BlockHeader, errores::NodoBitcoinError};
 
-use super::header::make_header;
+const MSG_BLOCK: u32 = 2;
 
-struct InventoryVector {
+pub struct Inventory {
     inv_type: u32,
     hash: [u8; 32],
 }
 
 pub struct GetDataMessage {
     count: u8,
-    inventory: Vec<InventoryVector>
+    inventory: Vec<Inventory>,
 }
 
 impl GetDataMessage {
-    pub fn new(inventory: Vec<InventoryVector>) -> GetDataMessage {
+    pub fn new(count: u8, hash: [u8; 32]) -> GetDataMessage {
+        let inventory = Inventory {
+            inv_type: MSG_BLOCK,
+            hash,
+        };
+
         GetDataMessage {
-            count: inventory.len() as u8,
-            inventory
+            count,
+            inventory: vec![inventory],
         }
     }
-    
+
     pub fn serialize(&self) -> Result<Vec<u8>, NodoBitcoinError> {
         let mut payload = Vec::new();
         let mut msg = Vec::new();
@@ -30,9 +36,10 @@ impl GetDataMessage {
             payload.extend_from_slice(&inventory.hash);
         }
 
-        let header = make_header(true, "getdata".to_string(), &payload)?;
+        let header = make_header("getdata".to_string(), &payload)?;
 
         msg.extend_from_slice(&header);
         msg.extend_from_slice(&payload);
+        Ok(msg)
     }
 }
