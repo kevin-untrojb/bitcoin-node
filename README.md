76b3601c1084a19e48ccea026b6fbb6abe64a0b6
danielspacek@gmail.com <dspacek@fi.uba.ar>
2023-05-26 23:39:31 -0300

    Inicio del GTK

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # 23C1-Los-Rustybandidos
-Repo for Rust Taller De Programacion 1 FIUBA
+Repo for Rust Taller De Programacion 1 FIUBA  
+  
+
+## Equipo Los Rustybandidos
+Schmidt Agustina  
+Spacek Daniel  
+Fraccaro Agustina  
+Untrojb Kevin  
+  
+  
+## Presentación de la entrega intermedia  
+  
+Slides:  https://docs.google.com/presentation/d/1IIWP1ySUBLSOf8tgvPSMC4lSymeUIUKowZAmug60pnU/edit?usp=sharing  
+
+
+
+## GTK  
+  
+Sigue los siguientes pasos para instalar la versión dev del GTK:
+
+* Instalar la biblioteca GTK 3. En Ubuntu debes ejecutar:  
+` sudo apt-get install libgtk-3-dev `
+* Para incluir la referencia en el proyecto, en caso de que no exista en el Cargo.toml, en la terminal de la carpeta del proyecto:  
+`cargo add gtk`  
+* Para ejecutar la aplicación:  
+`cargo run --bin {application_name}`  
+* En caso de que al ejecutar la aplicación, muestre un error de :  
+    - version `GLIBCXX_3.4.29' not found  
+
+    Debe limpiar la variable GTK_PATH desde la consola del IDE  
+    `unset GTK_PATH`  
+
+... y magia!  
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ mod protocol;
 use std::{env, println};
 
 use errores::NodoBitcoinError;
+use gtk::{
+    prelude::{ApplicationExt, ApplicationExtManual},
+    traits::{ButtonExt, ContainerExt, WidgetExt},
+    Align, Application, ApplicationWindow, Button,
+};
 
 use crate::{
     blockchain::node::Node,
@@ -16,6 +21,48 @@ use crate::{
 };
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    _ = config::inicializar(args);
+
+    let nombre_grupo = match config::get_valor("NOMBRE_GRUPO".to_string()) {
+        Ok(valor) => valor,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+
+    let title = format!("Nodo Bitcoin - {}", nombre_grupo).to_string();
+    let app = Application::builder()
+        .application_id("nodo_bitcoin")
+        .build();
+
+    app.connect_activate(move |app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(420)
+            .default_height(200)
+            .title(&title)
+            .build();
+
+        let button = Button::builder()
+            .label("Click me!")
+            .halign(Align::Center)
+            .valign(Align::Center)
+            .build();
+
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+
+        window.set_child(Some(&button));
+        window.show_all();
+    });
+
+    app.run();
+}
+
+fn old_main() {
     let args: Vec<String> = env::args().collect();
     let do_steps = || -> Result<(), NodoBitcoinError> {
         config::inicializar(args)?;
