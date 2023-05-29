713b2e5f0880865e012d159ce13af911a6c2362d
danielspacek@gmail.com <dspacek@fi.uba.ar>
2023-05-29 19:36:56 -0300

    branch de prueba 2

---
+++ src/protocol/blockchain_download.rs
@@ -0,0 +1,267 @@
+use crate::blockchain::block::{self, SerializedBlock};
+use crate::blockchain::node::Node;
+use crate::common::utils_timestamp::obtener_timestamp_dia;
+use crate::config;
+use crate::errores::NodoBitcoinError;
+use crate::messages::getdata::GetDataMessage;
+use crate::messages::getheaders::GetHeadersMessage;
+use crate::messages::headers::deserealize;
+use crate::messages::messages_header::check_header;
+use bitcoin_hashes::{sha256d, Hash};
+use std::sync::{Arc, Mutex};
+use std::{cmp, println, thread, vec};
+
+use super::admin_connections::AdminConnections;
+use super::connection::connect;
+
+pub const GENESIS_BLOCK: [u8; 32] = [
+    0x00, 0x00, 0x00, 0x00, 0x09, 0x33, 0xea, 0x01, 0xad, 0x0e, 0xe9, 0x84, 0x20, 0x97, 0x79, 0xba,
+    0xae, 0xc3, 0xce, 0xd9, 0x0f, 0xa3, 0xf4, 0x08, 0x71, 0x95, 0x26, 0xf8, 0xd7, 0x7f, 0x49, 0x43,
+];
+
+pub fn _version() -> Result<u32, NodoBitcoinError> {
+    let version = match (config::get_valor("VERSION".to_string())?).parse::<u32>() {
+        Ok(res) => res,
+        Err(_) => return Err(NodoBitcoinError::NoSePuedeLeerValorDeArchivoConfig),
+    };
+    Ok(version)
+}
+
+pub fn get_full_blockchain(
+    mut admin_connections: AdminConnections,
+) -> Result<(), NodoBitcoinError> {
+    let version = _version()?;
+    let start_block = GENESIS_BLOCK;
+    let get_headers = GetHeadersMessage::new(version, 1, start_block, [0; 32]);
+    let mut get_headers_message = get_headers.serialize()?;
+
+    let (connection, id) = admin_connections.find_free_connection()?;
+    connection.write_message(&get_headers_message)?;
+
+    loop {
+        let mut buffer = [0u8; 24];
+        let bytes_read_option = connection.read_message(&mut buffer)?;
+        match bytes_read_option {
+            Some(read_bytes) => {
+                if read_bytes == 0 {
+                    let (connection, _id) = match admin_connections.find_free_connection() {
+                        Ok(res) => res,
+                        Err(NodoBitcoinError::NoSeEncuentraConexionLibre) => {
+                            admin_connections = connect()?; // actualizo la lista de conexiones
+                            admin_connections.find_free_connection()?
+                        }
+                        Err(_) => continue,
+                    };
+                    connection.write_message(&get_headers_message)?;
+                    continue;
+                }
+            }
+            None => continue,
+        }
+
+        let valid_command: bool;
+        let (_command, headers) = match check_header(&buffer) {
+            Ok((command, payload_len)) => {
+                let mut headers = vec![0u8; payload_len];
+                connection.read_exact_message(&mut headers)?;
+                valid_command = command == "headers";
+                if valid_command && payload_len == 1 {
+                    break; // llegué al final de los headers
+                }
+                (command, headers)
+            }
+            Err(NodoBitcoinError::MagicNumberIncorrecto) => {
+                let (connection, _id) = admin_connections.find_free_connection()?;
+                connection.write_message(&get_headers_message)?;
+                continue;
+            }
+            Err(_) => continue,
+        };
+
+        if valid_command {
+            let blockheaders = deserealize(headers)?;
+            let fecha_inicial = config::get_valor("DIA_INICIAL".to_string())?;
+            let timestamp_ini = obtener_timestamp_dia(fecha_inicial);
+            let last_header = blockheaders[blockheaders.len() - 1];
+            let headers_filtrados: Vec<_> = blockheaders
+                .into_iter()
+                .filter(|header| header.time >= timestamp_ini)
+                .collect();
+
+            let n_threads_max: usize =
+                match (config::get_valor("CANTIDAD_THREADS".to_string())?).parse::<usize>() {
+                    Ok(res) => res,
+                    Err(_) => return Err(NodoBitcoinError::NoSePuedeLeerValorDeArchivoConfig),
+                };
+            let n_threads = cmp::min(n_threads_max, headers_filtrados.len());
+            let n_blockheaders_thread =
+                (headers_filtrados.len() as f64 / n_threads as f64).ceil() as usize;
+            let blocks = Arc::new(Mutex::new(vec![]));
+            let mut threads = vec![];
+
+            let admin_connections_mutex = Arc::new(Mutex::new(admin_connections.clone()));
+            println!(
+                "Descarga de headers. Total obtenidos: {:?}",
+                headers_filtrados.len()
+            );
+
+            for i in 0..n_threads {
+                let start: usize = i * n_blockheaders_thread;
+                let end: usize = start + n_blockheaders_thread;
+                let block_headers_thread =
+                    headers_filtrados[start..cmp::min(end, headers_filtrados.len())].to_vec();
+                let shared_blocks = blocks.clone();
+                let admin_connections_mutex_thread = admin_connections_mutex.clone();
+                threads.push(thread::spawn(move || {
+                    let (mut cloned_connection, mut thread_id_connection) =
+                        match admin_connections_mutex_thread.lock() {
+                            Ok(mut admin) => {
+                                let (thread_connection, thread_id_connection) =
+                                    match admin.find_free_connection() {
+                                        Ok((connection, id)) => (connection, id),
+                                        Err(_) => return,
+                                    };
+                                drop(admin);
+                                (thread_connection, thread_id_connection)
+                            }
+                            Err(_) => return,
+                        };
+
+                    for header in block_headers_thread {
+                        let hash_header = match header.serialize() {
+                            Ok(serialized_header) => serialized_header,
+                            Err(_) => continue,
+                        };
+
+                        let get_data = GetDataMessage::new(
+                            1,
+                            *sha256d::Hash::hash(&hash_header).as_byte_array(),
+                        );
+                        let get_data_message = match get_data.serialize() {
+                            Ok(res) => res,
+                            Err(_) => continue,
+                        };
+                        if cloned_connection.write_message(&get_data_message).is_err() {
+                            return;
+                        }
+                        loop {
+                            let mut change_connection: bool = false;
+                            let mut thread_buffer = [0u8; 24];
+
+                            let thread_bytes_read_result =
+                                cloned_connection.read_message(&mut thread_buffer);
+                            match thread_bytes_read_result {
+                                Ok(thread_bytes_read_option) => match thread_bytes_read_option {
+                                    Some(read_bytes) => {
+                                        if read_bytes == 0 {
+                                            change_connection = true;
+                                        }
+                                    }
+                                    None => continue,
+                                },
+                                Err(_) => return,
+                            }
+                            if change_connection {
+                                (cloned_connection, thread_id_connection) =
+                                    match admin_connections_mutex_thread.lock() {
+                                        Ok(mut admin) => {
+                                            let (thread_connection, thread_id_connection) =
+                                                match admin.change_connection(thread_id_connection)
+                                                {
+                                                    Ok((connection, id)) => (connection, id),
+                                                    Err(_) => continue,
+                                                };
+                                            drop(admin);
+                                            (thread_connection.clone(), thread_id_connection)
+                                        }
+                                        Err(_) => return,
+                                    };
+                                if cloned_connection.write_message(&get_data_message).is_err() {
+                                    return;
+                                }
+                                continue;
+                            }
+
+                            let valid_command: bool;
+                            let (_command, response_get_data) = match check_header(&thread_buffer) {
+                                Ok((command, payload_len)) => {
+                                    let mut response_get_data = vec![0u8; payload_len];
+
+                                    if cloned_connection
+                                        .read_exact_message(&mut response_get_data)
+                                        .is_err()
+                                    {
+                                        return;
+                                    }
+                                    valid_command = command == "block";
+                                    (command, response_get_data)
+                                }
+                                Err(NodoBitcoinError::MagicNumberIncorrecto) => {
+                                    (cloned_connection, thread_id_connection) =
+                                        match admin_connections_mutex_thread.lock() {
+                                            Ok(mut admin) => {
+                                                let (thread_connection, thread_id_connection) =
+                                                    match admin
+                                                        .change_connection(thread_id_connection)
+                                                    {
+                                                        Ok((connection, id)) => (connection, id),
+                                                        Err(_) => continue,
+                                                    };
+                                                drop(admin);
+                                                (thread_connection.clone(), thread_id_connection)
+                                            }
+                                            Err(_) => return,
+                                        };
+                                    if cloned_connection.write_message(&get_data_message).is_err() {
+                                        return;
+                                    }
+                                    continue;
+                                }
+                                Err(_) => {
+                                    continue;
+                                }
+                            };
+
+                            if valid_command {
+                                let mut cloned = shared_blocks.lock().unwrap();
+                                let block = match SerializedBlock::deserialize(&response_get_data) {
+                                    Ok(block) => block,
+                                    Err(_) => continue,
+                                };
+                                cloned.push(block);
+                                println!("Bloque #{} descargado", cloned.len());
+                                drop(cloned);
+                                break;
+                            }
+                        }
+                    }
+                }));
+            }
+
+            for thread in threads {
+                let _ = thread.join();
+            }
+
+            // guardar bloques
+            // save blocks
+            let bloques_a_guardar = blocks.lock();
+            if bloques_a_guardar.is_err() {
+                return Err(NodoBitcoinError::NoExisteArchivo);
+            }
+            for bloque in bloques_a_guardar.unwrap().iter() {
+                // guardar bloque
+            }
+
+            let get_headers = GetHeadersMessage::new(
+                version,
+                1,
+                *sha256d::Hash::hash(&last_header.serialize()?).as_byte_array(),
+                [0; 32],
+            );
+            get_headers_message = get_headers.serialize()?;
+            connection.write_message(&get_headers_message)?;
+        }
+    }
+
+    Ok(())
+}
