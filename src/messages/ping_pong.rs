7c0e2020dae90c960f07aa46698d80c7c81b5727
Agustina <aschmidt@fi.uba.ar>
2023-06-26 02:48:54 -0300

    cambio de clippy en ping pong

--- src/messages/ping_pong.rs
+++ src/messages/ping_pong.rs
@@ -7,6 +7,6 @@ pub fn make_pong(bytes: &[u8]) -> Result<Vec<u8>, NodoBitcoinError> {
     let mut msg = Vec::new();
     let header = make_header("pong".to_string(), &bytes.to_vec())?;
     msg.extend_from_slice(&header);
-    msg.extend_from_slice(&bytes);
+    msg.extend_from_slice(bytes);
     Ok(msg)
 }
