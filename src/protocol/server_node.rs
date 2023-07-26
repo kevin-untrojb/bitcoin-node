cf50f98f52881f6082fd5507100c8af8458d7a92
danielspacek@gmail.com <dspacek@fi.uba.ar>
2023-07-26 19:34:20 -0300

    inicio de listener

--- src/protocol/server_node.rs
+++ src/protocol/server_node.rs
@@ -1,37 +1,94 @@
 use std::{
-    io::{stdin, BufRead, BufReader, Read, Write},
+    io::{BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
-    thread::sleep,
+    sync::mpsc::Sender,
+    thread::{self, sleep},
     time::Duration,
 };
 
-use crate::{config, errores::NodoBitcoinError};
+use crate::{
+    config,
+    errores::NodoBitcoinError,
+    log::{log_error_message, log_info_message, LogMessages},
+};
 
-pub fn init_listener() -> Result<(), NodoBitcoinError> {
-    let port = match config::get_valor("PORT".to_owned()) {
-        Ok(res) => res,
-        Err(_) => "18333".to_owned(),
-    };
+pub fn init_server(logger: Sender<LogMessages>) -> Result<(), NodoBitcoinError> {
+    let port = config::get_valor("PORT".to_owned())?;
 
     let address = "127.0.0.1:".to_owned() + &port;
-    server_run(&address).unwrap();
+    server_run(&address, logger).unwrap();
     Ok(())
 }
 
-fn server_run(address: &str) -> std::io::Result<()> {
-    let listener = TcpListener::bind(address)?;
-    // accept devuelve una tupla (TcpStream, std::net::SocketAddr)
-    println!("Escuchando en: {:?}", listener.local_addr()?);
-    let (mut client_stream, socket_addr) = listener.accept()?;
-    println!("La socket addr del client: {:?}", socket_addr);
-    // let mut client_stream : TcpStream = connection.0;
-    // TcpStream implementa el trait Read, así que podemos trabajar como si fuera un archivo
-    handle_message(&mut client_stream)?;
+fn server_run(address: &str, logger: Sender<LogMessages>) -> Result<(), NodoBitcoinError> {
+    let listener = match TcpListener::bind(address) {
+        Ok(res) => res,
+        Err(_) => {
+            log_error_message(logger.clone(), "Error al bindear el socket".to_string());
+            return Err(NodoBitcoinError::NoSePudoConectar);
+        }
+    };
+
+    if listener.local_addr().is_err() {
+        log_error_message(
+            logger.clone(),
+            "Error al obtener la dirección local".to_string(),
+        );
+        return Err(NodoBitcoinError::NoSePudoConectar);
+    }
+
+    log_info_message(
+        logger.clone(),
+        format!("Escuchando en: {:?}", listener.local_addr().unwrap()),
+    );
+
+    loop {
+        match listener.accept() {
+            Ok((mut stream, socket_addr)) => {
+                log_info_message(
+                    logger.clone(),
+                    format!("Conexión establecida: {:?}", socket_addr),
+                );
+                thread::spawn(move || {
+                    handle_message(&mut stream);
+                });
+            }
+            Err(_) => {
+                log_error_message(logger.clone(), "Error al aceptar la conexión".to_string());
+                continue;
+            }
+        }
+    }
     Ok(())
 }
 
 //
 fn handle_message(stream: &mut TcpStream) -> std::io::Result<()> {
+    // handshake al revés
+    // read version
+    // write version
+    // read verack
+    // write verack
+
+    // si es ok el handshake, se hace el loop de recibir mensajes
+    // si no, se cierra el socket
+    // loop
+    // si el read sale por timeout, enviamos un ping, si no responde pong se cierra el socket
+    // si el read no lee bytes, se cierra el socket
+    // si el read lee bytes, se hace el parseo del mensaje
+
+    // los mensajes que podemos recibir son:
+    // - ping
+    // - pong
+    // - getheaders
+    // - getdata
+
+    // los mensajes que podemos enviar son:
+    // - ping
+    // - pong
+    // - headers
+    // - block
+
     loop {
         let mut buffer_read = [0 as u8; 100];
         let leidos = stream.read(&mut buffer_read).unwrap();
@@ -85,11 +142,14 @@ pub fn init_client(mensaje: String) -> Result<(), NodoBitcoinError> {
 
 #[cfg(test)]
 mod tests {
+    use crate::log::create_logger_actor;
+
     use super::*;
 
     #[test]
     fn test_run_server() {
-        init_listener();
+        let logger = create_logger_actor(config::get_valor("LOG_FILE".to_string()));
+        init_server(logger);
     }
 
     #[test]
